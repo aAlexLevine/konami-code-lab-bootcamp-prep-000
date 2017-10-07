d905447700bf4b536b52a9e22be7b7af515ac09b
Alex Levine <aalexlevine@gmail.com>
2017-10-07 15:07:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  const body = document.querySelector('body')
+
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    let index = 0;
+
+if (key === code[index]) {
+  index++
+
+}
+
+  })
+
 
 }
